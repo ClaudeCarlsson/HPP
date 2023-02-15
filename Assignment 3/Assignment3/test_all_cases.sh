8d8297c88949294abad43f4d3fefb34c94d5366a
ClaudeCarlsson <claudecarlsson@outlook.com>
2023-02-15 17:28:28 +0100

    Updated script

--- Assignment 3/Assignment3/test_all_cases.sh
+++ Assignment 3/Assignment3/test_all_cases.sh
@@ -1,18 +1,12 @@
 STEPS=200
 
-for i in 00010 00100 00500 01000 02000
+for i in 00010 00100 00500 01000 02000 03000
 do
-    echo -e '\n'
-    echo -e ${i} '------------------------------'
-    ./galsim ${i} input_data/ellipse_N_${i}.gal ${STEPS} 0.00001 0
-    sleep 0.5
-    compare_gal_files/compare_gal_files ${i} result.gal ref_output_data/ellipse_N_${i}_after${STEPS}steps.gal
+    if [ "$i" = "03000" ]
+    then 
+        STEPS=100
+    fi
+    echo -e "\n${i} ------------------------------"
+    ./galsim "${i}" input_data/ellipse_N_"${i}".gal "${STEPS}" 0.00001 0
+    compare_gal_files/compare_gal_files "${i}" result.gal ref_output_data/ellipse_N_"${i}"_after"${STEPS}"steps.gal
 done
-
-i=03000
-STEPS=100
-echo -e '\n'
-echo -e ${i} '------------------------------\n'
-./galsim ${i} input_data/ellipse_N_${i}.gal ${STEPS} 0.00001 0
-sleep 0.5
-compare_gal_files/compare_gal_files ${i} result.gal ref_output_data/ellipse_N_${i}_after${STEPS}steps.gal
